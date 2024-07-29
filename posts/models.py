db46b8b5d3f2f886808c07c2f663b769a06b51b4
jjungcu519 <jjungcu0519@gmail.com>
2024-07-29 11:08:49 +0900

    모델 정의

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.db import models #모듈
 
 # Create your models here.
+class Post(models.Model): # Model 상속 설정?
+    title = models.CharField(max_length=100) #title 에 글자가 들어올 수 있음을 정의해줌
